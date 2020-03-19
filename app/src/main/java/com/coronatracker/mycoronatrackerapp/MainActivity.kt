e5645356f9597c75a00dea816cfa858e3a2f4789
Thauvin Alexandre <alexandre.thauvin01@gmail.com>
2020-03-19 13:58:36 +0100

    implement crashlytics + perf + comment service

--- app/src/main/java/com/coronatracker/mycoronatrackerapp/MainActivity.kt
+++ app/src/main/java/com/coronatracker/mycoronatrackerapp/MainActivity.kt
@@ -27,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        checkDrawOverlayPermission()
+        //checkDrawOverlayPermission()
         initView()
         initListeners()
     }
