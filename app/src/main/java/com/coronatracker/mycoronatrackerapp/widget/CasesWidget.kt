53d685592042ad93e41d95fc9d6922f77cd1c0e8
Thauvin Alexandre <alexandre.thauvin01@gmail.com>
2020-03-20 20:45:14 +0100

    widget is working

--- app/src/main/java/com/coronatracker/mycoronatrackerapp/widget/CasesWidget.kt
+++ app/src/main/java/com/coronatracker/mycoronatrackerapp/widget/CasesWidget.kt
@@ -12,41 +12,43 @@ import com.coronatracker.mycoronatrackerapp.network.ApiServiceImp
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.*
+import kotlin.concurrent.schedule
 
 /**
  * Implementation of App Widget functionality.
  */
 class CasesWidget : AppWidgetProvider() {
     private val disposable = CompositeDisposable()
-    private lateinit var widgetListener: WidgetListener
 
-    interface WidgetListener {
-        fun updateData()
-    }
-    fun addListener(widgetListener: WidgetListener){
-        this.widgetListener = widgetListener
-    }
     override fun onUpdate(
         context: Context,
         appWidgetManager: AppWidgetManager,
         appWidgetIds: IntArray
     ) {
-        //widgetListener.updateData()
-        pullAll(context)
-        // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
-                .let { intent ->
-                    PendingIntent.getActivity(context, 0, intent, 0)
-                }
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.cases_widget).apply {
-                setOnClickPendingIntent(R.id.clRoot, pendingIntent)
-            }
+        disposable.add(
+            ApiServiceImp.getAll()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    for (appWidgetId in appWidgetIds) {
+                        val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+                            .let { intent ->
+                                PendingIntent.getActivity(context, 0, intent, 0)
+                            }
+                        // Construct the RemoteViews object
+                        val views = RemoteViews(context.packageName, R.layout.cases_widget).apply {
+                            setOnClickPendingIntent(R.id.clRoot, pendingIntent)
+                        }
+                        views.setTextViewText(R.id.tvNumberTotalConfirmedWidget, it.cases.toString())
+                        views.setTextViewText(R.id.tvNumberTotalDeathsWidget, it.deaths.toString())
+                        views.setTextViewText(R.id.tvNumberTotalRecoveredWidget, it.recovered.toString())
 
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }
+                        // Instruct the widget manager to update the widget
+                        appWidgetManager.updateAppWidget(appWidgetId, views)
+                    }
+                }, Throwable::printStackTrace)
+        )
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -55,20 +57,6 @@ class CasesWidget : AppWidgetProvider() {
         views.setTextViewText(R.id.tvNumberTotalConfirmedWidget, intent!!.getIntExtra(CONFIRMED, -1).toString())
         views.setTextViewText(R.id.tvNumberTotalConfirmedWidget, intent.getIntExtra(DEATHS, -1).toString())
         views.setTextViewText(R.id.tvNumberTotalConfirmedWidget, intent.getIntExtra(RECOVERED, -1).toString())
-    }
-
-    private fun pullAll(context: Context) {
-        val views = RemoteViews(context.packageName, R.layout.cases_widget)
-        disposable.add(
-            ApiServiceImp.getAll()
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    views.setTextViewText(R.id.tvNumberTotalConfirmedWidget, it.cases.toString())
-                    views.setTextViewText(R.id.tvNumberTotalDeathsWidget, it.deaths.toString())
-                    views.setTextViewText(R.id.tvNumberTotalRecoveredWidget, it.recovered.toString())
-                }, Throwable::printStackTrace)
-        )
     }
 
     override fun onEnabled(context: Context) {
